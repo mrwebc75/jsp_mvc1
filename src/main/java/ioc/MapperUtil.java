9ad5a55207d52a5961f8b9cbd20b206326be9d2d
안쌤 <mvcweb@naver.com>
2024-02-13 18:07:58 +0900

    mvc1

---
+++ src/main/java/ioc/MapperUtil.java
@@ -0,0 +1,21 @@
+package ioc;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+public enum MapperUtil {
+  INSTANCE;
+
+  private ModelMapper modelMapper;
+
+  private MapperUtil() {
+    this.modelMapper = new ModelMapper();
+    this.modelMapper.getConfiguration().setFieldMatchingEnabled(true)
+        .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+        .setMatchingStrategy(MatchingStrategies.STRICT);
+  }
+
+  public ModelMapper getModelMapper() {
+    return modelMapper;
+  }
+}
--- src/main/java/srv/UserServiceImpl.java
+++ src/main/java/srv/UserServiceImpl.java
@@ -2,7 +2,10 @@ package srv;
 
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
+
 import dao.UserMapper;
+import ioc.MapperUtil;
 import model.UserDTO;
 import model.UserVO;
 
@@ -10,9 +13,11 @@ public class UserServiceImpl implements UserService {
 
   // userDao 객체를 이용해서 작업
   private UserMapper userDao;
+  private ModelMapper modelMapper;
 
   public UserServiceImpl(UserMapper userDao) {
     this.userDao = userDao;
+    this.modelMapper = MapperUtil.INSTANCE.getModelMapper();
   }
 
   @Override
@@ -28,11 +33,13 @@ public class UserServiceImpl implements UserService {
     // 모델매퍼 라이브러리를 이용해 자동으로 변환
     if (vo != null) {
       // vo를 dto로 변환해서 리턴
-      dto = new UserDTO();
-      dto.setName(vo.getName());
-      dto.setPassword(vo.getPassword());
-      dto.setUserid(vo.getUserid());
-      dto.setRole(vo.getRole());
+      /*
+       * dto = new UserDTO(); dto.setName(vo.getName());
+       * dto.setPassword(vo.getPassword()); dto.setUserid(vo.getUserid());
+       * dto.setRole(vo.getRole());
+       */
+
+      dto = modelMapper.map(dto, UserDTO.class);
     }
 
     return dto;
